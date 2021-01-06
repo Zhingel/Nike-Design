b56d66fef34a61419ed25129505d50379dd26ffc
Stas <staszhingel@icloud.com>
2021-01-06 12:21:37 +0300

    Initial Commit

---
+++ Nike Design/Nike_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nike_DesignApp.swift
+//  Nike Design
+//
+//  Created by Стас Жингель on 06.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Nike_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nike DesignTests/Nike_DesignTests.swift
@@ -0,0 +1,33 @@
+//
+//  Nike_DesignTests.swift
+//  Nike DesignTests
+//
+//  Created by Стас Жингель on 06.01.2021.
+//
+
+import XCTest
+@testable import Nike_Design
+
+class Nike_DesignTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
